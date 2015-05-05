69eadcc66e2edb1e0823431042981c34b47c7bdf
Chris Lloyd <Chris@ChrisMBP.home>
2015-05-04 23:31:30 -0400

    Testing Pyglet

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import pyglet
+
 def main():
-	print("hello world")
+	window = pyglet.window.Window()
+	label = pyglet.text.Label('Hello, world',
+                          font_name='Times New Roman',
+                          font_size=36,
+                          x=window.width//2, y=window.height//2,
+                          anchor_x='center', anchor_y='center')
+	pyglet.app.run()
 
 main()
