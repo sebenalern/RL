f89b31d9c7bd95c7e5557385611ea9658dfe579b
Chris Lloyd <Chris@ChrisMBP.home>
2015-05-05 14:21:37 -0400

    Implemented map output

--- screen.py
+++ screen.py
@@ -5,35 +5,44 @@ window = pyglet.window.Window()
 
 class Screen:
 
-	chars = []
+	arr = []
 
 	def __init__(self):
 		global window
-		self.char_height = 36
-		self.char_width = 36
-		self.width = 10*self.char_width
-		self.height = 10*self.char_height
-		window.height = self.height
-		window.width = self.width
+		self.char_size = 18
+		self.width = 20
+		self.height = 20
+		window.height = self.height*self.char_size+5
+		window.width = self.width*self.char_size
 
-	def draw_char(self,char,x,y):
-		label = pyglet.text.Label(char,
-                      font_name='Times New Roman',
-                      font_size=36,
-                      x=x*self.char_width, y=y*self.char_height,
-                      anchor_x='center', anchor_y='center')
-		self.chars.append(label)
+		for y in range(self.height):
+			for x in range(self.width):
+				label = pyglet.text.Label("#",
+                      font_name='Courier',
+                      font_size=self.char_size,
+                      x=x*self.char_size, y=y*self.char_size,
+                      anchor_x='left', anchor_y='bottom')
+				self.arr.append(label)
+
+	def set_char(self,char,x,y):
+		self.arr[y*self.width+x].text = char
+
+	def clear_chars(self):
+		for label in self.arr:
+			label.text = " "
 
 screen = Screen()
-screen.draw_char("L",2,2)
+for y in range(5):
+	for x in range(5):
+		screen.set_char("L",x,y)
 
 @window.event
 def on_draw():
 	window.clear()
 	global screen
 	print("Test")
-	for c in screen.chars:
-		c.draw()
+	for label in screen.arr:
+		label.draw()
 
 pyglet.app.run()
 
